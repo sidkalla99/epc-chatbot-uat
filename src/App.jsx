3eb7d00c57a39f1f489f6fc2a521ce8adc673147
sidkalla99 <siddharth.kalla@zelestra.energy>
2025-06-22 18:40:22 +0530

    Update App.jsx
    increased padding

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1rem', paddingTop: '4rem'}}>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1rem', paddingTop: '8rem'}}>
             <h2 style={{ margin: 0 }}>Welcome, {user?.attributes?.email}</h2>
             <button onClick={signOut}>Sign Out</button>
           </div>
