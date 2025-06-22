a4d69264719840a832d78e0134c5179fb8ede1a2
sidkalla99 <siddharth.kalla@zelestra.energy>
2025-06-22 16:07:09 +0530

    Update vite.config.js
    modified the vite app config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',               // 👈 required for correct routing
+  build: {
+    outDir: 'dist'         // 👈 required so Amplify knows where to deploy from
+  }
 })
